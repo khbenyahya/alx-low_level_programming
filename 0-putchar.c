e4179772b508d23e08f12e3749c392ac214d5887
khbenyahya <khaoulabenyahya@gmail.com>
2023-03-26 06:29:50 -0700

    functionsnestedoos

---
+++ 0-putchar.c
@@ -0,0 +1,49 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@khbenyahya 
+Hamzaelaamili
+/
+alx-low_level_programming
+Public
+Fork your own copy of Hamzaelaamili/alx-low_level_programming
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-low_level_programming/0x02-functions_nested_loops/0-putchar.c
+@Hamzaelaamili
+Hamzaelaamili putchar
+Latest commit 5556220 5 days ago
+ History
+ 1 contributor
+19 lines (18 sloc)  249 Bytes
+
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
